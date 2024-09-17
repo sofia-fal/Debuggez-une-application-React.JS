00a0a250ffba7262b9bc59e70552087ddc60334a
sofia-fal <sofia-fal@hotmail.com>
2024-09-17 14:48:00 +0200

    Correction navigation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,7 +23,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section id="nos-services">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -52,11 +52,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section id="nos-realisations">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section id="notre-equipe">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
